ee3755de8920e39b2da803c887e6993b698d8f2f
Daluma Sen <daluma.sen@canada.ca>
2018-05-29 12:05:57 +0000

    fix python2.7 call for pyiotest

--- test-ssm.sh
+++ test-ssm.sh
@@ -14,7 +14,7 @@ bash -c 'echo "hoho" >> ./hoho'
 
 truncate --size=2 ./hoho
 
-/usr/bin/python2.7 pyiotest
+/usr/bin/env python pyiotest
 grep lovely pyiotest
 cp ${SR_POST_CONFIG} ~/test/hoho_my_darling.txt
 touch hihi
