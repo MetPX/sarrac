2a17a9c412ce20cf6493a29b90455212e2e02f48
Michael Saraga <mic.saraga@gmail.com>
2019-11-19 20:03:12 +0000

    is_utf8 -> sr_is_utf8

--- sr_util.c
+++ sr_util.c
@@ -190,13 +190,13 @@ void log_set_fnts()
 	}
 }
 
-/* is_utf8     routine to confirm that a field is utf8 encoded, taken verbatim from:
+/* sr_is_utf8     routine to confirm that a field is utf8 encoded, taken verbatim from:
  *             https://stackoverflow.com/questions/1031645/how-to-detect-utf-8-in-plain-c 
  *             considered CC-BY-SA.  Is this a sufficient attribution?
  *             https://legalict.com/2016/01/07/what-is-the-license-status-of-stackoverflow-code-snippets/
  */
 
-int is_utf8(const char *string)
+int sr_is_utf8(const char *string)
 {
 	if (!string)
 		return 0;
