fc88b28126ccab439bc0414d0784a03d32c05aaa
PeterSilva <peter@bsqt.homeip.net>
2022-12-17 10:41:15 -0500

    ran check_shim_test.py through yapf3

--- check_shim_test.py
+++ check_shim_test.py
@@ -2,93 +2,100 @@
 
 import sys
 
-bad=0
-good=0
-post_count={}
-tests=0
+bad = 0
+good = 0
+post_count = {}
+tests = 0
 
 # by default run through all and print summary at end.
 # with this option, exit on first failed test.
 
-exit_on_bad = ( len(sys.argv) >= 3 ) and sys.argv[2] == 'exit_on_bad'
+exit_on_bad = (len(sys.argv) >= 3) and sys.argv[2] == 'exit_on_bad'
 
-test_algo=None
-with open( sys.argv[1], 'r') as log:
-   l = log.readlines()
-   #print(l)
-   for i in l:
+test_algo = None
+with open(sys.argv[1], 'r') as log:
+    l = log.readlines()
+    #print(l)
+    for i in l:
 
-      line = i.split()
+        line = i.split()
 
-      if (i[0] == '+') and line[1] != 'echo':
+        if (i[0] == '+') and line[1] != 'echo':
             print(i)
             continue
 
-      if len(line) < 4:
-         continue
+        if len(line) < 4:
+            continue
 
-      # sample
-      # 2022-12-15 09:54:30,666 [DEBUG] post_broker: amqp://tfeed:<pw>@localhost:5672
+        # sample
+        # 2022-12-15 09:54:30,666 [DEBUG] post_broker: amqp://tfeed:<pw>@localhost:5672
 
-      #skip lines like this:
-      #  ++ truncate --size=2 ./hoho
-      if line[0] == '#test':
-         tests += 1
-         if test_algo:  # finish previous test...
+        #skip lines like this:
+        #  ++ truncate --size=2 ./hoho
+        if line[0] == '#test':
+            tests += 1
+            if test_algo:  # finish previous test...
 
-             if len(test_actual_posts) == 0:
-                print( f"RESULT: BAD! missing expected {test_post_count} for {test_description}" )
-                bad += 1
-             for m in test_actual_posts:
-                if m in test_post_count and (test_post_count[m]==test_actual_posts[m]):
-                     print( f"RESULT: Good! {test_post_count[m]} {m} posts from {test_description}. as expected" )
-                     good += 1
-                else:
-                     print( f"RESULT: BAD! {test_actual_posts[m]} {m} posts, expected: {test_post_count} for {test_description}" )
-                     bad += 1
+                if len(test_actual_posts) == 0:
+                    print(
+                        f"RESULT: BAD! missing expected {test_post_count} for {test_description}"
+                    )
+                    bad += 1
+                for m in test_actual_posts:
+                    if m in test_post_count and (test_post_count[m]
+                                                 == test_actual_posts[m]):
+                        print(
+                            f"RESULT: Good! {test_post_count[m]} {m} posts from {test_description}. as expected"
+                        )
+                        good += 1
+                    else:
+                        print(
+                            f"RESULT: BAD! {test_actual_posts[m]} {m} posts, expected: {test_post_count} for {test_description}"
+                        )
+                        bad += 1
 
-             if bad and exit_on_bad:
-                 sys.exit(1) 
-             
-         #print( f"setting: {line} " )
-         test_algo=line[2]
-         
-         test_post_count={}
-         test_post_count[test_algo] = int(line[1])
- 
-         test_description=' '.join(line[3:])
-         test_actual_count=0
-         test_actual_posts={}
-         
+                if bad and exit_on_bad:
+                    sys.exit(1)
 
-      if line[3] == 'published:' :
-         print(i)
-         # 2022-12-15 09:54:30,677 [INFO] published: 20221215145430.676464601 sftp://peter@localhost /home/peter/Sarracenia/metpx-sr3c/hoho topic=v03.post.home.peter.Sarracenia.metpx-sr3c sum=n,ac837d7b87a8ca12f0343401d66d081d source=tfeed to_clusters=localhost from_cluster=localhost mtime=20221215145430.67089267 atime=20221215145430.658892516 mode=0664 parts=1,5,1,0,0 toto=pig
+            #print( f"setting: {line} " )
+            test_algo = line[2]
 
-         if line[10] == '"method"':
-             sum_algo=line[12].replace('"','').replace(':','').replace("'",'').replace(',','')
-         else:
-             sum_algo=line[12].replace('"','').replace(':','').replace("'",'').replace(',','')
+            test_post_count = {}
+            test_post_count[test_algo] = int(line[1])
 
-         #sum_algo=line[8][4]
-         if sum_algo in test_actual_posts:
-            test_actual_posts[ sum_algo ] += 1
-         else: 
-            test_actual_posts[ sum_algo ] = 1
+            test_description = ' '.join(line[3:])
+            test_actual_count = 0
+            test_actual_posts = {}
 
-         if sum_algo in post_count:
-            post_count[ sum_algo ] += 1
-         else: 
-            post_count[ sum_algo ] = 1
+        if line[3] == 'published:':
+            print(i)
+            # 2022-12-15 09:54:30,677 [INFO] published: 20221215145430.676464601 sftp://peter@localhost /home/peter/Sarracenia/metpx-sr3c/hoho topic=v03.post.home.peter.Sarracenia.metpx-sr3c sum=n,ac837d7b87a8ca12f0343401d66d081d source=tfeed to_clusters=localhost from_cluster=localhost mtime=20221215145430.67089267 atime=20221215145430.658892516 mode=0664 parts=1,5,1,0,0 toto=pig
 
-   print( f"posts: {post_count}" )
+            if line[10] == '"method"':
+                sum_algo = line[12].replace('"', '').replace(':', '').replace(
+                    "'", '').replace(',', '')
+            else:
+                sum_algo = line[12].replace('"', '').replace(':', '').replace(
+                    "'", '').replace(',', '')
 
-   
-   print( f"RESULT: summary: good: {good}, bad {bad}, total {tests}" )
+            #sum_algo=line[8][4]
+            if sum_algo in test_actual_posts:
+                test_actual_posts[sum_algo] += 1
+            else:
+                test_actual_posts[sum_algo] = 1
 
-   if bad:
-       sys.exit(bad)
-   if good < tests:
-       sys.exit(tests-good)
+            if sum_algo in post_count:
+                post_count[sum_algo] += 1
+            else:
+                post_count[sum_algo] = 1
 
-   sys.exit(0)
+    print(f"posts: {post_count}")
+
+    print(f"RESULT: summary: good: {good}, bad {bad}, total {tests}")
+
+    if bad:
+        sys.exit(bad)
+    if good < tests:
+        sys.exit(tests - good)
+
+    sys.exit(0)
