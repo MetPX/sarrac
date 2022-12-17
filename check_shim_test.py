8961b3fbd4c6272a3fcb850eeb82315c5c8599ed
PeterSilva <peter@bsqt.homeip.net>
2022-12-17 01:10:09 -0500

    more progress on shim library testing.

--- check_shim_test.py
+++ check_shim_test.py
@@ -2,10 +2,24 @@
 
 import sys
 
+bad=0
+good=0
+post_count={}
+tests=0
+
+# by default run through all and print summary at end.
+# with this option, exit on first failed test.
+exit_on_bad = sys.argv[2] == 'exit_on_bad'
+
+def finish_test():
+    global bad
+    global good
+
+
+test_algo=None
 with open( sys.argv[1], 'r') as log:
    l = log.readlines()
-   print(l)
-   post_count={}
+   #print(l)
    for i in l:
       line = i.split()
 
@@ -18,13 +32,44 @@ with open( sys.argv[1], 'r') as log:
       #skip lines like this:
       #  ++ truncate --size=2 ./hoho
       if line[0] == '#test':
+         tests += 1
+         if test_algo:  # finish previous test...
+             for m in test_actual_posts:
+                if m in test_post_count and ( test_post_count[m]==test_actual_posts[m]):
+                     print( f"RESULT: Good! {test_post_count[m]} {m} posts from {test_description}. as expected" )
+                     good += 1
+                else:
+                     print( f"RESULT: BAD! {test_actual_posts[m]} {m} posts from {test_description} as expected" )
+                     if exit_on_bad:
+                        sys.exit(1) 
+                     bad += 1
+             
          print( f"setting: {line} " )
+         test_algo=line[2]
+         
+         test_post_count={}
+         test_post_count[test_algo] = int(line[1])
+ 
+         test_description=' '.join(line[3:])
+         test_actual_count=0
+         test_actual_posts={}
+         
 
       if line[3] == 'published:' :
          print(i)
          # 2022-12-15 09:54:30,677 [INFO] published: 20221215145430.676464601 sftp://peter@localhost /home/peter/Sarracenia/metpx-sr3c/hoho topic=v03.post.home.peter.Sarracenia.metpx-sr3c sum=n,ac837d7b87a8ca12f0343401d66d081d source=tfeed to_clusters=localhost from_cluster=localhost mtime=20221215145430.67089267 atime=20221215145430.658892516 mode=0664 parts=1,5,1,0,0 toto=pig
-         #print( f'sum: {line[8]}')
-         sum_algo=line[8][4]
+
+         if line[10] == '"method"':
+             sum_algo=line[12].replace('"','').replace(':','').replace("'",'').replace(',','')
+         else:
+             sum_algo=line[12].replace('"','').replace(':','').replace("'",'').replace(',','')
+
+         #sum_algo=line[8][4]
+         if sum_algo in test_actual_posts:
+            test_actual_posts[ sum_algo ] += 1
+         else: 
+            test_actual_posts[ sum_algo ] = 1
+
          if sum_algo in post_count:
             post_count[ sum_algo ] += 1
          else: 
@@ -32,25 +77,12 @@ with open( sys.argv[1], 'r') as log:
 
    print( f"posts: {post_count}" )
 
-   expected_post_count = { 's' : ( 0, 'sha512 sum posts' ), 'L' : ( 2, 'symoblic links'), 'n': ( 10, 'none' ) , 'R': ( 6, 'file removals' ) }
+   
+   print( f"RESULT: summary: good: {good}, bad {bad}, total {tests}" )
 
+   if bad:
+       sys.exit(bad)
+   if good < tests:
+       sys.exit(tests-good)
 
-   bad=0
-   good=0
-   for e in expected_post_count:
-      epi = expected_post_count[e]
- 
-      if e in post_count:
-          actual = post_count[e]
-      else:
-          actual = 0
-
-      if actual != expected_post_count[e][0]:
-          bad+=1
-          print( f'ERROR, for {expected_post_count[e][1]} expected: {expected_post_count[e][0]}, got {actual}' )
-      else:
-          good+=1
-          print( f'GOOD, got {actual} for {expected_post_count[e][1]} as expected' )
-
-   print( f"summary: good: {good}, bad {bad}, total {good+bad}" )
-   sys.exit(bad)
+   sys.exit(0)
--- shim_test.sh
+++ shim_test.sh
@@ -6,47 +6,51 @@ if [ ! "${SR_POST_CONFIG}" ]; then
    exec $0
 fi
 
+if [ ! -d ~/test ]; then
+  mkdir ~/test
+fi
 set -x
 
 which bash
 
 echo "FIXME: KNOWN ISSUE redirection close does not get posted!"
-echo "#test 1 n capturing stdout"
+echo "#test 1 sha512 capturing stdout"
 bash -c 'echo "hoho" >> ./hoho'
 
-echo "#test 1 n c program run."
+echo "#test 1 sha512 c program run."
 truncate --size=2 ./hoho
 
-echo "#test 1 n python program run"
+echo "#test 1 sha512 python program run"
 /usr/bin/python2.7 pyiotest
 grep lovely pyiotest
 
-echo "#test 1 n cp command.
+echo "#test 1 sha512 cp command"
 cp libsr3shim.c ~/test/hoho_my_darling.txt
 echo "#test 1 n touch command"
 touch hihi
-echo "#test 1 L symlink command
+echo "#test 1 link symlink command"
 ln -s hoho haha
-echo "#test 1 L moving a symlink"
+echo "#test 1 link moving a symlink"
 mv haha hihi
-echo "#test 1 L hardlink to a symlink
+echo "#test 1 hardlink hardlink to a symlink
 ln hihi hoohoo
-echo "#test 1 n moving a file. "
+echo "#test 1 sha512 moving a file. "
 mv ~/test/hoho_my_darling.txt ~/test/hoho2.log
-echo "#test 1 R removing a file. 
+echo "#test 1 remove removing a file. 
 rm hihi
 
-echo "#test 1 R removing a file." 
+echo "#test 1 remove removing a file." 
 rm ~/test/hoho2.log
 mkdir dirone
-echo "#test 1 n stdout redirection in a subdir
+echo "#test 1 sha512 stdout redirection in a subdir
 echo "fileone" >>dirone/fileone
 mkdir dirone/dirtwo
-echo "#test 1 n stdout redirection in a subsubdir"
+echo "#test 1 sha512 stdout redirection in a subsubdir"
 echo "filetwo" >>dirone/dirtwo/filetwo
-echo "#test 1 n renaming subdirs should cause file rename events.
+echo "#test 1 sha512 renaming subdirs should cause file rename events.
 mv dirone dirthree
-echo "#test 1 n removing a whole tree events."
+echo "#test 1 sha512 removing a whole tree events."
 rm -rf dirthree
-echo "#test 2 R removing two files"
+echo "#test 2 remove removing two files"
 rm hoho hoohoo
+echo "#test 0 last test to ensure previous completes"
