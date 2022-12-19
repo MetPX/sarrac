675a0be185ccf4664de9de54074609736d7e9a5c
PeterSilva <peter@bsqt.homeip.net>
2022-12-19 09:04:58 -0500

    fixups for shim_test.sh

--- shim_test.sh
+++ shim_test.sh
@@ -2,7 +2,13 @@
 if [ ! -d ~/test ]; then
   mkdir ~/test
 fi
-rm hihi hoho haha ~/test/hoho_my_darling.txt hoohoo
+
+echo "ignore rm errors.. cleaning before start"
+rm hihi 
+rm hoho 
+rm haha 
+rm ~/test/hoho_my_darling.txt 
+rm hoohoo
 
 set -x
 
@@ -18,41 +24,49 @@ fi
 which bash
 
 echo "FIXME: KNOWN ISSUE redirection close does not get posted!"
-#echo "#test 1 sha512 capturing stdout"
-#bash -c 'echo "hoho" >> ./hoho'
+echo "#test 1 sha512 capturing stdout"
+bash -c 'echo "hoho" >> ./hoho'
 
-#echo "#test 1 sha512 c program run."
-#truncate --size=2 ./hoho
+echo "#test 1 sha512 c program run."
+truncate --size=2 ./hoho
 
-#echo "#test 1 sha512 python program run"
-#/usr/bin/python2.7 pyiotest
-#grep lovely pyiotest
+echo "#test 1 sha512 python program run"
+/usr/bin/python2.7 pyiotest
+
+grep lovely pyiotest
 
 echo "#test 1 sha512 cp command"
 cp libsr3shim.c ~/test/hoho_my_darling.txt
+
 echo "#test 1 sha512 touch command"
 touch hihi
+
 echo "#test 1 link symlink command"
 ln -s hoho haha
+
 echo "#test 1 link moving a symlink"
 mv haha hihi
 
-echo "#test 1 hardlink hardlink to a symlink
+echo "#test 1 hardlink hardlink to a symlink"
 ln hihi hoohoo
 echo "#test 1 sha512 moving a file. "
 mv ~/test/hoho_my_darling.txt ~/test/hoho2.log
-echo "#test 1 remove removing a file. 
+echo "#test 1 remove removing a file. "
 rm hihi
 
 echo "#test 1 remove removing a file." 
 rm ~/test/hoho2.log
+
+
 mkdir dirone
-echo "#test 1 sha512 stdout redirection in a subdir
+echo "#test 1 sha512 stdout redirection in a subdir"
 echo "fileone" >>dirone/fileone
 mkdir dirone/dirtwo
+
 echo "#test 1 sha512 stdout redirection in a subsubdir"
 echo "filetwo" >>dirone/dirtwo/filetwo
-echo "#test 1 sha512 renaming subdirs should cause file rename events.
+
+echo "#test 1 sha512 renaming subdirs should cause file rename events."
 mv dirone dirthree
 echo "#test 1 sha512 removing a whole tree events."
 rm -rf dirthree
