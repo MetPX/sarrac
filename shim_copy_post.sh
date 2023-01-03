9b70c6a2c4b68bed910fa80f383699852e2398a3
Peter Silva <peter@bsqt.homeip.net>
2023-01-03 11:31:26 -0500

    more complete mirror test (all test cases uncommented now.)

--- shim_copy_post.sh
+++ shim_copy_post.sh
@@ -20,37 +20,34 @@ grep lovely pyiotest
 echo "#test 1 sha512 cp command"
 cp ../libsr3shim.c hoho_my_darling.txt
 
-#echo "#test 1 sha512 touch command"
-#touch hihi
+echo "#test 1 sha512 touch command"
+touch hihi
 
-#echo "#test 1 link symlink command"
-#ln -s hoho haha
+echo "#test 1 link symlink command"
+ln -s hoho haha
 
-#echo "#test 1 rename moving a symlink"
-#mv haha hihi
+echo "#test 1 rename moving a symlink"
+mv haha hihi
 
-#echo "#test 1 rename hardlink to a symlink"
-#ln hihi hoohoo
-#echo "#test 1 rename moving a file. "
-#mv hoho_my_darling.txt hoho2.log
+echo "#test 1 rename hardlink to a symlink"
+ln hihi hoohoo
+echo "#test 1 rename moving a file. "
+mv hoho_my_darling.txt hoho2.log
 
-#echo "#test 1 remove removing a file." 
-#rm hoho2.log
+echo "#test 1 remove removing a file." 
+rm hoho2.log
 
 
-#mkdir dirone
-#echo "#test 1 sha512 stdout redirection in a subdir"
-#echo "fileone" >>dirone/fileone
-#mkdir dirone/dirtwo
+mkdir dirone
+echo "#test 1 sha512 stdout redirection in a subdir"
+echo "fileone" >>dirone/fileone
+mkdir dirone/dirtwo
 
-#echo "#test 1 sha512 stdout redirection in a subsubdir"
-#echo "filetwo" >>dirone/dirtwo/filetwo
+echo "#test 1 sha512 stdout redirection in a subsubdir"
+echo "filetwo" >>dirone/dirtwo/filetwo
 
-#echo "#test 2 rename renaming subdirs should cause file rename events."
-#mv dirone dirthree
+echo "#test 2 rename renaming subdirs should cause file rename events."
+mv dirone dirthree
 #echo "#test 0 last test to ensure previous completes"
 
-export JOBSTEP=2
-unset LD_PRELOAD
-export SR_SHIMDEBUG=0
 echo "#test 0 comment shim copy posting end"
