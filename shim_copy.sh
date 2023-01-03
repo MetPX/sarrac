4bccbb9c61d639c7c758c5b77315c892b66746e9
Peter Silva <peter@bsqt.homeip.net>
2023-01-03 10:54:44 -0500

    improved mirroring test case.

--- shim_copy.sh
+++ shim_copy.sh
@@ -17,8 +17,60 @@ for d in shim_dirA shim_dirB; do
 done 
 mkdir -p ~/.config/sr3/subscribe
 mkdir -p ~/.config/sr3/cpost
-cp local_copy.conf ~/.config/sr3/subscribe
-cp local_post.conf ~/.config/sr3/cpost
+
+#cp local_copy.conf ~/.config/sr3/subscribe
+
+BROKER=amqp://tfeed@localhost
+EXCHANGE=xs_feed
+
+cat >~/.config/sr3/subscribe/local_copy.conf <<EOT
+
+broker ${BROKER}
+exchange ${EXCHANGE}
+
+topicPrefix v03.post
+subtopic #
+logReject on
+logLevel debug
+debug True
+expire 10m
+nodupe_ttl 0
+logMessageDump on
+callback log
+
+directory `pwd`/shim_dirB
+accept .*
+
+EOT
+
+#cp local_post.conf ~/.config/sr3/cpost
+cat >~/.config/sr3/cpost/local_post.conf  <<EOT
+
+post_broker ${BROKER}
+post_exchange ${EXCHANGE}
+
+vip 127.0.0.1
+realpath off
+logReject on
+logLevel debug
+debug True
+exchangeDeclare True
+#integrity none
+shim_post_minterval 10
+#shim_skip_parent_open_files
+#shim_post_once
+#shim_defer_posting_to_exit
+expire 1d
+nodupe_ttl 0
+header toto=pig
+events modify,link,delete
+
+post_base_url file:/
+post_topicPrefix v03.post
+
+accept .*
+EOT
+
 sr3 declare cpost/local_post
 sr3 declare subscribe/local_copy
 sr3 start subscribe/local_copy.conf
@@ -28,7 +80,7 @@ export LD_PRELOAD=`pwd`/libsr3shim.so.1.0.0
 export LD_LIBRARY_PATH=`pwd`:${LD_LIBRARY_PATH}
 export SR_SHIMDEBUG=99
 cd shim_dirA
-../shimcopy1.sh &
+../shim_copy_post.sh &
 unset SR_POST_CONFIG
 unset SR_SHIMDEBUG
 unset LD_PRELOAD
