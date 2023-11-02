11ab6a67ff41e43508fbbf954e9347e02ba2720c
Peter Silva <peter@bsqt.homeip.net>
2023-11-01 20:57:09 -0400

    move metrics according to https://github.com/MetPX/sarracenia/issues/793

--- sr_config.c
+++ sr_config.c
@@ -1768,11 +1768,11 @@ int sr_config_finalize(struct sr_config_s *sr_cfg, const int is_consumer)
 	}
 	// MetricsFilename
 	if (val) {
-		sprintf(p, "%s/.cache/%s/%s/%s/%s/i%03d.metrics", home, sr_cfg->appname,
+		sprintf(p, "%s/.cache/%s/%s/metrics/%s_%s_%03d.json", home, sr_cfg->appname,
 			val, sr_cfg->progname, sr_cfg->configname, sr_cfg->instance);
 	}
 	else {
-		sprintf(p, "%s/.cache/%s/%s/%s/i%03d.metrics", home, sr_cfg->appname,
+		sprintf(p, "%s/.cache/%s/metrics/%s_%s_%03d.json", home, sr_cfg->appname,
 			sr_cfg->progname, sr_cfg->configname, sr_cfg->instance);
 	}
 	sr_cfg->metricsFilename = strdup(p);
