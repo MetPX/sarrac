c638c9fa6ecdbc7e2ae90d8d02de56c9802b4b60
Peter Silva <peter@bsqt.homeip.net>
2023-12-20 21:52:26 -0500

    fix shim_copy_strip_slash.sh test that is failing

--- shim_copy_strip_slash.sh
+++ shim_copy_strip_slash.sh
@@ -87,7 +87,7 @@ nodupe_ttl 0
 header toto=pig
 events modify,link,delete,mkdir,rmdir
 
-post_baseUrl sftp://${USER}@localhost
+post_baseUrl sftp://${USER}@localhost/
 post_topicPrefix v03.post
 
 accept `realpath .`/.*
