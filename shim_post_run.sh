e7dc1fef10a0e7349056511fe2fae91d0017d5aa
Peter Silva <peter@bsqt.homeip.net>
2024-11-08 11:53:10 -0500

    adding gdb run to test failure

--- shim_post_run.sh
+++ shim_post_run.sh
@@ -7,6 +7,10 @@ echo "#test 0 comment 000 shim test posting start"
 echo "#test 1 sha512 070 cp command"
 cp libsr3shim.c ~/test/hoho_my_darling.txt
 
+echo "#test 1 sha512 070 cp command"
+gdb cp <<EOT
+run libsr3shim.c ~/test/hoho_my_darling.txt
+EOT
 
 echo "#test 1 sha512 070 cp command"
 valgrind  --show-reachable=yes --track-origins=yes --leak-check=full cp libsr3shim.c ~/test/hoho_my_darling.txt
