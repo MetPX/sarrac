aedaee2eb6741d1bb76ff6eafd61840f69bb57b0
Reid Sunderland <reid.sunderland@ssc-spc.gc.ca>
2025-03-27 20:48:15 +0000

    need realpath here too

--- shim_copy_mirror.sh
+++ shim_copy_mirror.sh
@@ -87,7 +87,7 @@ nodupe_ttl 0
 header toto=pig
 events modify,link,delete,mkdir,rmdir
 
-post_baseUrl file:`pwd`/shim_dirA
+post_baseUrl file:`realpath $(pwd)`/shim_dirA
 post_topicPrefix v03.post
 
 accept `realpath .`/.*
